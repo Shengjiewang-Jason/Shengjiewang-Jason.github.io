a5e9fdf99d795ba5dc5f56b5ed2f5f37756fbd97
Shengjie-bob <wangsj19@mails.tsinghua.edu.cn>
2022-05-12 14:34:50 +0800

    update gifs

--- mp42gif.py
+++ mp42gif.py
@@ -1,9 +1,9 @@
 import moviepy.editor as mpy
 
 # 视频文件的本地路径
-content = mpy.VideoFileClip("./iros.mp4")
+content = mpy.VideoFileClip("./ral.mp4")
 # 剪辑0分0秒到0分4秒的片段。resize为修改清晰度
-c1 = content.subclip((0,0), (0, 5)).resize((600, 337))
+c1 = content.subclip((0,0), (0, 5)).resize((600, 540))
 # 将片段保存为gif图到python的默认路径
-c1.write_gif("./iros.gif",fps=5)
+c1.write_gif("./ral.gif",fps=5)
 
